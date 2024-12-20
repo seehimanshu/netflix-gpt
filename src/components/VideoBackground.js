4b5d7bc693de000d2592221ae08e5805e96980e3
seehimanshu <himanshu.thakur2812@gmail.com>
2024-12-20 19:48:24 +0530

    made some changes

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -9,7 +9,7 @@ const VideoBackground = ({movieId}) => {
     useMovieTrailer(movieId);
     
   return (
-    <div className='w-screen'>
+    <div className=''>
         <iframe 
             className=' w-screen aspect-video'
             src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
